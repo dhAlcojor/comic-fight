b449dfd52a0c91b77858e78902bbca998432316f
Daniel H. Alcojor <dhalcojor@gmail.com>
2024-08-19 13:21:47 +0200

    Add code coverage reporter to Karma config
    Updated the Karma configuration to include the "coverage" reporter. This change ensures that code coverage reports are generated alongside the existing progress and HTML reporters, aiding in better analysis and testing of the project's codebase.

--- karma.conf.js
+++ karma.conf.js
@@ -41,7 +41,7 @@ module.exports = function (config) {
         },
       },
     },
-    reporters: ["progress", "kjhtml"],
+    reporters: ["progress", "kjhtml", "coverage"],
     browsers: ["Chrome"],
     restartOnFileChange: true,
   })
